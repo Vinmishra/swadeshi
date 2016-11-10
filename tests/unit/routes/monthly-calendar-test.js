714ec96fc7cc6554573b9aa0e624dd8286c155dd
vinmishra <vindhya.mishra@jpmchase.com>
2016-11-10 13:26:58 +0530

    first commit

---
+++ tests/unit/routes/monthly-calendar-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:monthly-calendar', 'Unit | Route | monthly calendar', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
